06e8179c2d8f23755d01663dfc0c16db61934bec
Emin Tuncer <memin11235@gmail.com>
2025-01-11 14:30:11 +0300

    Create project
    İnitial Commit

---
+++ production-planning/src/main/java/com/huawei/productplanning/ProductionPlanningApplication.java
@@ -0,0 +1,10 @@
+package com.huawei.productplanning;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionPlanningApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionPlanningApplication.class, args);
+	}
+}
